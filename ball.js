1d4083ce53852ba4d4df5bd9ff2dde89ee0afa1d
drankerus <43856122+drankerus@users.noreply.github.com>
2019-03-28 19:56:18 +0200

    Add files via upload

--- ball.js
+++ ball.js
@@ -1,16 +1,86 @@
 class Ball{
-  constructor(color,number,x,y,speed){
+  constructor(color,number,x,y,a){
     this.color = color;
     this.number = number;
-    this.x = x;
-    this.y=  y;
-    this.speed = speed;
+    this.x = x || Math.round(Math.random()*50);
+    this.y=  y || Math.round(Math.random()*50);;
+    this.a = a || Math.round(Math.random()*360);;
+    this.speedX = 0
+    this.speedY = 0;
+    this.diameter = 20
   }
   render(){
-    var html = `<div class="ball ball--${this.color}" style="position: absolute; left: ${this.x}px; top:${this.y}px;" >
+    var html = `<div id="b-${this.number}" class="ball ball--${this.color}" style="position: absolute; left: ${this.x}px; top:${this.y}px;" >
         <div>  ${this.number} </div>
 
     </div>`
     document.getElementById("table").innerHTML += html;
+    var that = this;
+    setInterval(function(){
+      that.move()
+    },50)
+  }
+  move(){
+    if(this.isAtBottom()){
+      this.speedY *= -1
+    }
+    if(this.isAtTop()){
+      this.speedY *= -1
+    }
+    if(this.isAtLeft()){
+      this.speedX *= -1
+    }
+    if(this.isAtRight()){
+      this.speedX *= -1
+    }
+
+    if (this.speedX>0 && this.speedX< table.w-this.x){
+      this.x += this.speedX
+    }else if(this.speedX > 0 ){
+      this.x += table.h-this.x
+    }
+    if (this.speedX<0 && this.speedX> -this.x){
+      this.x += this.speedX
+    }else if(this.speedX < 0 ){
+      this.x += this.x
+    }
+    if (this.speedY>0 && this.speedY< table.h-this.y){
+      this.y += this.speedY
+    }else if(this.speedY > 0 ){
+      this.y += table.h-this.y
+    }
+    if (this.speedY<0 && this.speedY> -this.y){
+      this.y += this.speedY
+    }else if(this.speedY < 0 ){
+      this.y += this.y
+    }
+
+    this.a += this.speedY+this.speedX
+
+    this.speedX *= 0.9
+    this.speedY *= 0.9
+
+    var ball = document.getElementById(`b-${this.number}`)
+      ball.style.left = `${this.x}px`
+      ball.style.top = `${this.y}px`
+      ball.style.transform = `rotate(${this.a}deg)`
+    //console.log(this.x,this.y);
+  }
+  push(sx,sy){
+    this.speedX = sx;
+    this.speedY = sy;
+
+  }
+  isAtBottom(){
+    return this.y >= table.h - this.diameter
+  }
+  isAtTop(){
+    return this.y <= 0
+  }
+  isAtLeft(){
+    return this.x <= 0
+  }
+  isAtRight(){
+    return this.x >= table.w - this.diameter
   }
 }
--- billy.js
+++ billy.js
@@ -1,11 +1,11 @@
 var table = new Table(1100,600,50);
 
 // add balls
-table.addBall(new Ball("red",1,20,20,0))
-table.addBall(new Ball("blue",1,20,20,0))
-table.addBall(new Ball("orange",1,20,20,0))
-table.addBall(new Ball("purple",1,20,20,0))
-table.addBall(new Ball("black",1,20,20,0))
+table.addBall(new Ball("red",1,20,20))
+table.addBall(new Ball("blue",2,20,20))
+table.addBall(new Ball("orange",3,20,20))
+table.addBall(new Ball("purple",4,20,20))
+table.addBall(new Ball("black",5,20,20))
 
 
 table.render()
